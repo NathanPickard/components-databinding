af76ddd35f1460f1e04491371d3a7090fd9819aa
Nathan Pickard <nathan.pickard87@gmail.com>
2018-03-25 20:57:23 -0700

    Project scaffolding

---
+++ src/app/sever-element/sever-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sever-element',
+  templateUrl: './sever-element.component.html',
+  styleUrls: ['./sever-element.component.css']
+})
+export class SeverElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
